153e0ab8d82cb1d67b448f4bc8a8e3635bdd33fd
vibhor <vbharadwaj@vibhors-mbp.attlocal.net>
2015-06-28 21:38:37 -0700

    Base App for Theme Switcher exercise

--- app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
@@ -1,48 +1,39 @@
 package com.codepath.android.themeswitcher.app;
 
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Spinner;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
-public class ThemeActivity extends AppCompatActivity {
-    private Spinner spThemes;
+public class ThemeActivity extends ActionBarActivity {
 
-    // Here we set the theme for the activity
-    // Note `Utils.onActivityCreateSetTheme` must be called before `setContentView`
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // MUST BE SET BEFORE setContentView
-        Utils.onActivityCreateSetTheme(this);
-        // AFTER SETTING THEME
         setContentView(R.layout.activity_theme);
-        setupSpinnerItemSelection();
     }
 
-    private void setupSpinnerItemSelection() {
-        spThemes = (Spinner) findViewById(R.id.spThemes);
-        spThemes.setSelection(ThemeApplication.currentPosition);
-        ThemeApplication.currentPosition = spThemes.getSelectedItemPosition();
 
-        spThemes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_theme, menu);
+        return true;
+    }
 
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int position, long id) {
-                if (ThemeApplication.currentPosition != position) {
-                    Utils.changeToTheme(ThemeActivity.this, position);
-                }
-                ThemeApplication.currentPosition = position;
-            }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
 
-            }
-        });
-
+        return super.onOptionsItemSelected(item);
     }
 }
