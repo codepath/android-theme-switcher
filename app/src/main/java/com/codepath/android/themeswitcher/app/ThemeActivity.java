680bab440df76ff22fda2ef073303e3e2decdb43
Roger Hu <roger.hu@gmail.com>
2017-10-16 22:29:18 -0700

    Upgrade versions
    Delete the .iml file

--- app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
@@ -1,12 +1,12 @@
 package com.codepath.android.themeswitcher.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ThemeActivity extends ActionBarActivity {
+public class ThemeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
